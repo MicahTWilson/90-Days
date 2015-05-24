4c75a25c24468a66dfde432620d7f9cfe028a5e0
Micah Wilson <MicahW@Micahs-MBP.lan>
2015-05-23 18:00:15 -0600

    Initial Commit

---
+++ 90 Day/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  90 Day
+//
+//  Created by Micah Wilson on 5/23/15.
+//  Copyright (c) 2015 Micah Wilson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
